6496fc17d65d652065b488d7076223e9da927301
wattai <evamark.wattai@gmail.com>
2022-04-06 01:22:25 +0900

    Update

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from scream.scream import scream
+from scream.scream import scream as scream_fn
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, World! and {scream()}</p>"
+    return f"<p>Hello, World!</p>"
 
 @app.route("/scream")
 def scream():
-    return f"<p>{scream()}</p>"
+    return f"<p>{scream_fn()}</p>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
